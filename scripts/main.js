4115686764f4a93dd12e8635eaf112414264b816
Sam Jones <77613903+ACSableIOM@users.noreply.github.com>
2021-03-14 11:38:17 +0000

    Mainpage getting there
    main page hard bits have been coded, just now adding extra posts to make the page a little longer

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mainNav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mainNav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}
