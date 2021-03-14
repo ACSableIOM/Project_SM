813a2251a256db9ace401d56dbd4fa7f392f74fa
Sam Jones <77613903+ACSableIOM@users.noreply.github.com>
2021-03-14 15:55:58 +0000

    Main page Finished
    Main page has been finished with mobile and desktop css completed.
    
    JavaScript added to help with the usability of the website and all webpages have been linked together.

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,28 @@ function closeNav() {
     document.getElementById("mainNav").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
 }
+
+// Inspiration from https://www.w3schools.com/howto/howto_css_modal_images.asp
+var popout = document.getElementById("mainPopout")
+
+// image retrieval
+var mainImg = document.getElementById("attImgs");
+var images = document.getElementsByClassName('attImg')
+var popImg = document.getElementById("modImg");
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    //click listener
+    img.onclick = function(evt){
+        popout.style.display = "block";
+        popImg.src = this.src;
+        console.info(evt);
+    }
+}
+
+
+//close larger modal
+var clos = document.getElementsByClassName("closeBtn")[0];
+
+clos.onclick = function(){
+    popout.style.display= "none";
+}
